ee0440d6705c971c39d1c4aeaca4603146f0999a
Filip Joelsson <filip@pixiecoder.com>
2019-05-24 00:28:02 +0200

    Added two more e2e tests

--- test/e2e/spa-general.test.js
+++ test/e2e/spa-general.test.js
@@ -8,7 +8,7 @@ let driver;
 
 const PORT = 8081;
 const baseUrl = `localhost:${PORT}/`;
-const timeout = 2000;
+const timeout = 500;
 
 describe('html tests', () => {
   beforeAll((done) => {
@@ -32,6 +32,44 @@ describe('html tests', () => {
       .then((element) => element.getAttribute('id'))
       .then((id) => {
         expect(id).toBe('animal-listing');
+        done();
+      });
+  });
+
+  test('populate select', (done) => {
+    driver.wait(until.elementLocated(By.id('animal-select')), timeout)
+      .then((select) => driver.wait(until.elementIsVisible(select)))
+      .then((select) => {
+        select.click();
+        return select;
+      })
+      .then((select) => select.findElements(By.tagName('option')))
+      .then((options) => options[0])
+      .then((option) => option.click())
+      .then(() => driver.wait(until.elementLocated(By.id('animal-description')), timeout))
+      .then((description) => driver.wait(until.elementIsVisible(description), timeout))
+      .then((description) => description.getAttribute('data-loaded'))
+      .then((dataLoaded) => {
+        expect(dataLoaded).toBe('true');
+        done();
+      });
+  });
+
+  test('upload animal', (done) => {
+    driver.wait(until.elementLocated(By.id('animal-to-add')), timeout)
+      .then((textarea) => {
+        textarea.sendKeys('\t');
+        textarea.clear();
+        textarea.sendKeys('{"name":"test"}');
+      })
+      .then(() => driver.wait(until.elementLocated(By.id('animal-add')), timeout))
+      .then((button) => button.click())
+      .then(() => driver.wait(until.elementLocated(By.id('animal-select')), timeout))
+      .then((select) => select.findElements(By.tagName('option')))
+      .then((options) => options.pop())
+      .then((lastOption) => lastOption.getAttribute('data-name'))
+      .then((dataName) => {
+        expect(dataName).toBe('test');
         done();
       });
   });
